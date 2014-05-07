5263658cf83ce07c600f7163645fc12efe1bb980
Steve Shogren <steve.a.shogren@gmail.com>
2014-05-07 07:14:03 -0400

    Finishing the inverted poly post, and now refactoring for DI is bad

--- TestPatterns/Patterns.cs
+++ TestPatterns/Patterns.cs
@@ -10,23 +10,30 @@ namespace Database
 		void Add(IPayment payment);
 	}
 	public class InMemory : IPaymentRepository {
+		public Dictionary<int, IPayment> payments = new Dictionary<int, IPayment>();
 		public void Add(IPayment payment) {
-			Config.payments.Add(payment.GetId(),payment);
+			payments.Add(payment.GetId(),payment);
 		}
 		public IEnumerable<IPayment> GetAll (){
-			return Config.payments.Values;
+			return payments.Values;
 		}
 	}
 	public class Postgres : IPaymentRepository {
 		public void Add(IPayment payment) {
-			//Connection.InsertRecord(payment);
+			throw new NotImplementedException();
 		}
 		public IEnumerable<IPayment> GetAll (){
-			//Connection.GetAll<IPayment>();
 			throw new NotImplementedException();
 		}
 	}
+	public interface IDBConnection { }
+	public class DBConnection : IDBConnection{ }
 	public class RepositoryFactory {
+		IDBConnection con;
+		public RepositoryFactory(IDBConnection con) {
+			this.con = con;
+		}
+		public RepositoryFactory() : this(new DBConnection()) { }
 		public static IPaymentRepository GetPaymentRepo ()
 		{
 			if (Config.configuration["database"] == "inmemory") {
@@ -36,6 +43,7 @@ namespace Database
 			}
 		}
 	}
+
 
 	public class User {
 		public static void Use() {
--- monads-fsharp/Program.fs
+++ monads-fsharp/Program.fs
@@ -1,36 +1,30 @@
-
-// NOTE: If warnings appear, you may need to retarget this project to .NET 4.0. Show the Solution
-// Pad, right-click on the project node, choose 'Options --> Build --> General' and change the target
-// framework to .NET 4.0 or .NET 4.5.
-
 module monadsfsharp.DatabaseFSharp
 
 open System
 open Helpers
+open System.Collections.Generic
 
-type Repositories = 
-   | InMemory
+type Repository = 
+   | InMemory of Dictionary<int, IPayment>
    | Postgres
 
 let Add db (payment:IPayment) = 
     match db with
-        | InMemory -> Config.payments.Add(payment.GetId(),payment);
+        | InMemory payments -> payments.Add(payment.GetId(),payment);
         | Postgres -> raise(NotImplementedException())
-let GetAll db = 
-    match db with
-        | InMemory -> Config.payments.Values;
-        | Postgres -> raise(NotImplementedException())
+        
+let GetAll = function
+    | InMemory payments -> payments.Values;
+    | Postgres -> raise(NotImplementedException())
         
 let GetPaymentRepo = 
     match Config.configuration.["database"] with
-        | "inmemory" -> InMemory   
-        | "postgres" -> Postgres   
-        
+        | "inmemory" -> InMemory(new Dictionary<int, IPayment>())
+        | "postgres" -> Postgres 
      
 [<EntryPoint>]
 let main args = 
     let repo = GetPaymentRepo
     let payments = GetAll repo
-    Console.WriteLine("Converted to " +  "")
     0
 
